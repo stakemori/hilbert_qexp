7a5a9e8ab16bca0c461866404714a549f2d3b86f
Sho Takemori <stakemorii@gmail.com>
2018-02-06 11:29:50 +0100

    Test weights

--- hilbert_sqrt2/tests/test.rs
+++ hilbert_sqrt2/tests/test.rs
@@ -6,24 +6,28 @@ mod parallel {
     #[test]
     fn test_s4() {
         let s4 = s4_form(5);
+        assert_eq!(s4.weight, Some((4, 4)));
         println!("{}", s4);
     }
 
     #[test]
     fn test_s6() {
         let s6 = s6_form(5);
+        assert_eq!(s6.weight, Some((6, 6)));
         println!("{}", s6);
     }
 
     #[test]
     fn test_s5() {
         let s5 = s5_form(5);
+        assert_eq!(s5.weight, Some((5, 5)));
         println!("{}", s5);
     }
 
     #[test]
     fn test_s9() {
         let s9 = s9_form(5);
+        assert_eq!(s9.weight, Some((9, 9)));
         println!("{}", s9);
     }
 }
