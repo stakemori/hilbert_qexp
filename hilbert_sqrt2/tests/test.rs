83bd18376f7d302e8329c549eba734aedcdcca8e
Sho Takemori <stakemorii@gmail.com>
2018-02-15 14:41:49 +0100

    Modify tests

--- hilbert_sqrt2/tests/test.rs
+++ hilbert_sqrt2/tests/test.rs
@@ -8,28 +8,36 @@ mod parallel {
     fn test_s4() {
         let s4 = s4_form(5);
         assert_eq!(s4.weight, Some((4, 4)));
-        println!("{}", s4);
+        for (i, a) in s4.diagonal_restriction().iter().enumerate() {
+            println!("{}: {}", i, a);
+        }
     }
 
     #[test]
     fn test_s6() {
         let s6 = s6_form(5);
         assert_eq!(s6.weight, Some((6, 6)));
-        println!("{}", s6);
+        for (i, a) in s6.diagonal_restriction().iter().enumerate() {
+            println!("{}: {}", i, a);
+        }
     }
 
     #[test]
     fn test_s5() {
         let s5 = s5_form(5);
         assert_eq!(s5.weight, Some((5, 5)));
-        println!("{}", s5);
+        for (i, a) in s5.diagonal_restriction().iter().enumerate() {
+            println!("{}: {}", i, a);
+        }
     }
 
     #[test]
     fn test_s9() {
         let s9 = s9_form(5);
         assert_eq!(s9.weight, Some((9, 9)));
-        println!("{}", s9);
+        for (i, a) in s9.diagonal_restriction().iter().enumerate() {
+            println!("{}: {}", i, a);
+        }
     }
 }
 
